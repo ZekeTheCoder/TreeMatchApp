3f30b2524d2d771aafa02f31f6105286dc7e9479
Thabang Maphangisane <thabangh647@gmail.com>
2024-09-14 00:22:11 +0200

    feat: Add a user model to server

--- server/models.py
+++ server/models.py
@@ -32,3 +32,34 @@ class Plant(db.Model):
         self.title = title
         self.description = description
         db.session.commit()
+
+
+# user model
+
+"""
+class User:
+    id:integer
+    username:string
+    email:string
+    password:string
+"""
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), nullable=False, unique=True)
+    email = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.Text(), nullable=False)
+
+    def __repr__(self):
+        """
+        returns string rep of user object
+        """
+        return f"<User {self.username}>"
+
+    def save(self):
+        """
+        save user object in database
+        """
+        db.session.add(self)
+        db.session.commit()
