d04a4b355690435eaafd9a88b57e75a067d1b8b1
ZekeTheCoder <Thabangh647@gmail.com>
2024-09-16 20:45:27 +0200

    simple React app that renders a message from state.

---
+++ client/src/Index.js
@@ -0,0 +1,13 @@
+import React, {useEffect, useState} from "react";
+import ReactDOM from "react-dom"
+
+const App=()=>{
+	const [message, setMessage]=useState('')
+	return(
+		< div className="app">
+			{message}
+		</div>
+	)
+}
+
+ReactDOM.render(<App/>,document.getElementById('root'));
