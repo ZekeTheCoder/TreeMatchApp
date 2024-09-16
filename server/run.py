ceacd3d86f35e93fad297c11138664fbd4af00f3
Thabang Maphangisane <thabangh647@gmail.com>
2024-09-16 17:34:19 +0200

    feat: initializes and runs the Flask application. The application is created using the configuration passed.

---
+++ server/run.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+run.py - This module initializes and runs the Flask application.
+-----------------------------------------------------------------------------
+It imports the `create_app` function from the `main` module and configuration
+classes from the `config` module.
+The application is created using the `DevConfig` configuration by default,
+but can be switched to use `ProdConfig` or `TestConfig'.
+
+To run the application, execute this script directly. The application will 
+start on port 5000.
+
+Usage:
+	python run.py
+"""
+from main import create_app
+from config import DevConfig  # , ProdConfig, TestConfig
+
+app = create_app(DevConfig)
+# app = create_app(ProdConfig)
+# app = create_app(TestConfig)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
