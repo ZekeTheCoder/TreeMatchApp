38c27df961c85bd0e72e54d53538afb5ae3047e9
ZekeTheCoder <Thabangh647@gmail.com>
2024-09-17 22:53:48 +0200

    added normal routes for landing page. protected routes wth user auth and catch all not found routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,57 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import './App.css'
+import './App.css';
+// import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { Login } from "./components1/Login";
+import LoginPage from "./components/LoginPage";
+import Header from "./components/Header";
+import { Home } from "./components1/Home";
+import LandingPage from './components/LandingPage';
+import SignUpPage from './components/SignUpPage';
+// import { Register } from "./components1/Register";
+// import Map from './components/Map'; //
+// import Soil from './components/Soil';
+import Trees from './components/Trees';
+import { isLoggedIn } from './utils/auth';
+import NotFound from './components/NotFound';
+import PlantIdentify from './components/PlantIdentify';
+import PlantSearch from './components/PlantSearch';
+// import Profile from './components/Profile';
 
 function App() {
-  const [users, setUsers] = useState([])
 
-//   function to fetch data from the API
-const fetchData = async () => {
-	const response = await axios.get('http://localhost:5000/api/users'); // https://jsonplaceholder.typicode.com/posts
-	// console.log(response.data.users);
-	setUsers(response.data.users);
-};
 
-useEffect(() => {
-	fetchData();
-}, []);
+	// Get login status
+	const loggedIn = isLoggedIn();
+	console.log('loggedIn: ' + loggedIn);
 
-  return (
-    <>
-      <h1>TreeMatch</h1>
-		{users.map((user, index) => (
-			<div key={index}>
-				<span>{user}</span>
-				{/* <h3>{user.name}</h3> */}
-				{/* <p>{user.email</p> */}
-				<br />
-				</div>
-		))}
-    </>
-  )
+	return (
+		<>
+			<BrowserRouter>
+				<Header />
+
+				<Routes>
+					{/* <Route path="/" element={<Home />} /> */}
+					<Route path="/" element={<LandingPage />} />
+					<Route path="/login" element={<LoginPage />} />
+					{/* <Route path="/register" element={<Register />} /> */}
+					<Route path="/signup" element={<SignUpPage />} />
+					{/* Protected Routes */}
+					{loggedIn && (
+						<>
+							<Route path="/trees" element={<Trees />} />
+							<Route path="/plant_identify" element={<PlantIdentify />} />
+							<Route path="/plant_search" element={<PlantSearch />} />
+							{/* <Route path="/map" element={<Map />} />
+							<Route path="/soil" element={<Soil />} />
+							<Route path="/profile" element={<Profile />} /> */}
+						</>
+					)}
+
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
-export default App
+export default App;
---
+++ client/src/components/LandingPage.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import axios from "axios";
+import { Navigate } from "react-router-dom";
+
+const LandingPage = () => {
+
+	return (
+		<div className="form-signin mt-5 text-center">
+			<h3>Welcome to TreeMatch Landing Page</h3>
+		</div>);
+}
+
+export default LandingPage;
