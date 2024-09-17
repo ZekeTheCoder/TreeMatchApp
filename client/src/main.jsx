daf62056e0c972d1399bbd4fd990ca7e0a09b662
ZekeTheCoder <Thabangh647@gmail.com>
2024-09-17 22:41:26 +0200

    add: bootstrap react dependencies for styling

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,12 @@
+// Rendering logic
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.jsx'
-import './index.css'
+// import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<App />
+	</StrictMode>,
 )
